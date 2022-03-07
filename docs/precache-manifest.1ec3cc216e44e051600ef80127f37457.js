31ef14414e8cb3245d985bf2d20a4214610eeb23
alfredom7 <alfredoalejandromedina@gmail.com>
2022-03-07 12:41:07 -0500

    add docs folder

---
+++ docs/precache-manifest.1ec3cc216e44e051600ef80127f37457.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a0865cee7f0b1926b9e2891a33bd46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc182682f4fc39758228",
+    "url": "/static/css/main.1ece0064.chunk.css"
+  },
+  {
+    "revision": "22594595bc3b4452a224",
+    "url": "/static/js/2.d3a27f31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d3a27f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc182682f4fc39758228",
+    "url": "/static/js/main.360696d0.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
